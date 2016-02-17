9c6fd8e29ab9be6d9af9bc4404b90f94e20e9975
Sebastian Pozoga <sebastian@pozoga.eu>
2016-02-17 19:20:49 +0100

    finish base

--- goat.go
+++ goat.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"os"
-
-	"errors"
+	//	"flag"
 	"fmt"
-	"strings"
-	"time"
+	"os"
+	"github.com/goatcms/goat/src/command"
 )
 
 var (
@@ -19,25 +15,24 @@ var (
 const (
 	// The version of this tool
 	version = "0.0.1"
-
-	// Used for outputting console messages
-	logDivider = "\n------\n"
 )
 
 func main() {
-	if len(args) < 2 {
-		showHelp()
+	if len(os.Args) < 2 {
+		command.HelpCommand(nil, version)
 		return
 	}
 
-	command := os.Args[1]
-	switch command {
+	switch os.Args[1] {
 
-	case "version", "v":
-		showVersion()
+	case "new":
+		command.NewCommand(os.Args[2:])
 
-	case "help", "h", "wat", "?":
-		showHelp(args)
+	case "version":
+		command.VersionCommand(os.Args[2:], version)
+
+	case "help":
+		command.HelpCommand(os.Args[2:], version)
 
 	default:
 		consoleError("Unknow command")
@@ -45,24 +40,7 @@ func main() {
 
 }
 
-func showVersion() {
-	msg := logDivider
-	msg += fmt.Sprintf("Goat version: %s", version)
-	msg += logDivider
-	log.Print(msg)
-}
-
-
 func consoleError(msg string) {
-	log.Fatal(msg)
+	fmt.Println("Error: " + msg)
 	os.Exit(1)
-}
-
-func showHelp() {
-	help := logDivider
-	help += fmt.Sprintf("Goat version: %s", version)
-	help += "\n  goat version -> display version"
-	help += "\n  goat help -> display help"
-	help += logDivider
-	log.Print(help)
 }
